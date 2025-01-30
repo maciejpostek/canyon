59b46d312e29e5466bdc0720c98be6e9a535e580
maciejpostek <hello@maciejpostek.com>
2025-01-30 09:55:29 +0100

    Full config

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: globals.browser,
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'warn',
+      eqeqeq: 'error',
+      curly: 'error',
+    },
+  },
+  pluginJs.configs.recommended,
+];
